e05cab78a0f14f2e3825188e2fa2ada1c03b000a
lucianoseraim <lucianoserafimdesouza@gmail.com>
2014-08-13 09:25:26 -0300

    Criação das classes de excessões padrão.

---
+++ src/br/com/slv/util/DAOException.java
@@ -0,0 +1,35 @@
+package br.com.slv.util;
+
+public class DAOException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3066364117539580475L;
+
+	public DAOException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DAOException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DAOException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DAOException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
